38c2a06c80e35696094afd2b0f5c22661dd15310
NeginSal <neginsaljooghi@gmail.com>
2021-07-17 11:45:42 +0430

    add home component

---
+++ src/pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container mt-5">
+      <div class="row">
+          <div class="col-md-6">
+              <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Atque temporibus unde esse quia pariatur vitae nostrum nihil? Quasi sed dicta inventore.
+                  Reiciendis magni tempora incidunt delectus atque omnis architecto quibusdam!
+              </p>
+
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import { createRouter,createWebHistory } from 'vue-router';
+import Home from './pages/Home.vue'
 
-const routes = [];
+const routes = [
+    {path:'/', name:Home, component:Home}
+];
 
 const router = createRouter({
     history: createWebHistory(),
