e079a368ee8edf0d3489eed2cd4d5b4daa27cba6
NeginSal <neginsaljooghi@gmail.com>
2021-07-18 16:45:15 +0430

    add axios

--- src/pages/users/Index.vue
+++ src/pages/users/Index.vue
@@ -1,11 +1,27 @@
 <template>
   <div>
-      
+      <h1>
+          users
+      </h1>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+    setup(){
+        function getUsers(){
+            axios
+            .get("https://jsonplaceholder.typicode.com/users")
+            .then(function(response){
+                console.log(response);
+            })
+            .catch(function(error){
+                console.log(error)
+            });
+        }
+        getUsers()
+    },
 
 }
 </script>
