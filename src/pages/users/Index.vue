76af407c88b0460e710a9c077ae197d503ae05bf
NeginSal <neginsaljooghi@gmail.com>
2021-07-19 10:41:50 +0430

    add CardView component

--- src/pages/users/Index.vue
+++ src/pages/users/Index.vue
@@ -1,26 +1,28 @@
 <template>
   <div>
-      <h1>
-          users
-      </h1>
+     <h1>{{users}}</h1>
+
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import { ref } from '@vue/reactivity';
 export default {
     setup(){
+        const users = ref([]);
         function getUsers(){
             axios
             .get("https://jsonplaceholder.typicode.com/users")
             .then(function(response){
-                console.log(response);
+                users.value = response.data;
             })
             .catch(function(error){
                 console.log(error)
             });
         }
         getUsers()
+        return users;
     },
 
 }
