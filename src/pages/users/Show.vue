06518722e6ae385bdd20bcb09989c9e300ec0567
NeginSal <neginsaljooghi@gmail.com>
2021-07-20 11:02:43 +0430

    add Show.vue component

---
+++ src/pages/users/Show.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container mt-5">
+      <div class="row " >
+          <div class="col-md-4 my-2">
+              <UserCardView :user="user"/>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { ref } from '@vue/reactivity';
+import UserCardView from '@/components/users/CardView'
+import { useRoute } from "vue-router";
+
+export default {
+    components:{
+       UserCardView
+    },
+    setup(){
+        const user = ref({});
+        const route = useRoute();
+
+        function getUser(){
+            axios
+            .get(`https://jsonplaceholder.typicode.com/users/${route.params.id}`)
+            .then(function(response){
+                user.value = response.data;
+            })
+            .catch(function(error){
+                console.log(error)
+            });
+        }
+        getUser()
+        return {user};
+    },
+
+}
+</script>
+
+<style>
+
+</style>
--- src/router.js
+++ src/router.js
@@ -1,10 +1,13 @@
 import { createRouter,createWebHistory } from 'vue-router';
 import Home from './pages/Home.vue'
 import IndexUser from './pages/users/Index.vue'
+import ShowUser from './pages/users/Show.vue'
 
 const routes = [
     {path:'/', name:Home, component:Home},
-    {path:'/users', name:"users", component:IndexUser},
+    {path:'/users', name:"users", component:IndexUser , children:[
+        {path: ':id', name: 'userId', component: ShowUser}
+    ]},
 ];
 
 const router = createRouter({
