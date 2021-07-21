73f383c209685d92765585d9464df20a7a71c51e
NeginSal <neginsaljooghi@gmail.com>
2021-07-21 11:44:36 +0430

    adding index component to posts folder

---
+++ src/pages/posts/Index.vue
@@ -0,0 +1,47 @@
+<template>
+
+          <div class="col-md-4 my-2" v-for="post in posts" :key="post.id">
+            <div class="card">
+            <div class="card-header">
+               <!--<router-link :to="{ name : 'postId' , params :{ id : post.id }}"> -->
+                 {{ post.title }}
+                  <!-- </router-link>-->
+             </div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Body: {{ post.body }}</li>
+              </ul>
+            </div>
+          </div>
+
+</template>
+
+<script>
+import axios from 'axios'
+import { ref } from '@vue/reactivity';
+import { useRoute } from 'vue-router';
+
+export default {
+    setup(){
+        const posts = ref([]);
+        const route = useRoute()
+
+        function getPosts(){
+            axios
+            .get("https://jsonplaceholder.typicode.com/posts")
+            .then(function(response){
+                posts.value = response.data;
+            })
+            .catch(function(error){
+                console.log(error)
+            });
+        }
+        getPosts()
+        return {posts, route};
+    },
+
+}
+</script>
+
+<style>
+
+</style>
